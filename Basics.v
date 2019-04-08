95b41731499f615cc4052d450f3601b150f22f40
drzix <korea.drzix@gmail.com>
2019-04-08 11:44:14 +0900

    Basics: Exercise(zero_nbeq_plus_1) 추가
    Signed-off-by: drzix <korea.drzix@gmail.com>

--- Basics.v
+++ Basics.v
@@ -1251,7 +1251,10 @@ Qed.
 Theorem zero_nbeq_plus_1 : forall n : nat,
   0 =? (n + 1) = false.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n. destruct n as [|n'] eqn:En.
+  - reflexivity.
+  - reflexivity.
+Qed.
 (** [] *)
 
 (* ================================================================= *)
