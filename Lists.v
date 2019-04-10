2dae09b8143f13966e2ccbbcb5dae90c903eb858
drzix <korea.drzix@gmail.com>
2019-04-10 20:47:20 +0900

    Lists: Exercise(snd_fst_is_swap, fst_swap_is_snd) 추가
    Signed-off-by: drzix <korea.drzix@gmail.com>

--- Lists.v
+++ Lists.v
@@ -134,14 +134,14 @@ Proof.
 Theorem snd_fst_is_swap : forall (p : natprod),
   (snd p, fst p) = swap_pair p.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros p. destruct p. reflexivity. Qed.
 (** [] *)
 
 (** **** Exercise: 1 star, standard, optional (fst_swap_is_snd)  *)
 Theorem fst_swap_is_snd : forall (p : natprod),
   fst (swap_pair p) = snd p.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros p. destruct p. reflexivity. Qed.
 (** [] *)
 
 (* ################################################################# *)
