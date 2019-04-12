22547d2e8a56f6245107ca7a57a193a08787a035
drzix <korea.drzix@gmail.com>
2019-04-12 20:25:50 +0900

    Poly: Exercise(flat_map) 추가
    Signed-off-by: drzix <korea.drzix@gmail.com>

--- Poly.v
+++ Poly.v
@@ -843,13 +843,16 @@ Qed.
 *)
 
 Fixpoint flat_map {X Y: Type} (f: X -> list Y) (l: list X)
-                   : (list Y)
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+                   : (list Y) :=
+  match l with
+  | [] => []
+  | h :: t => f h ++ (flat_map f t)
+  end.
 
 Example test_flat_map1:
   flat_map (fun n => [n;n;n]) [1;5;4]
   = [1; 1; 1; 5; 5; 5; 4; 4; 4].
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 (** [] *)
 
 (** Lists are not the only inductive type for which [map] makes sense.
